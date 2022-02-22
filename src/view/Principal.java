8d0006b2b0b10230a658bd72b863bea13a252b4c
emaci <emaci@DESKTOP-EDU>
2022-02-21 23:22:17 -0300

    update

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -4,8 +4,12 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		loop();
 
+	}
+	
+	public static void loop() {
+		loop();
 	}
 
 }
